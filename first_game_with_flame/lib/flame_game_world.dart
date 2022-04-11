73ac0d9b607a1f950a03f2c02802a45beb135335
Tobias <tobiasra@stud.ntnu.no>
2022-04-11 20:08:27 +0200

    Adding the world graphics & fixed camera position

--- first_game_with_flame/lib/flame_game_world.dart
+++ first_game_with_flame/lib/flame_game_world.dart
@@ -1,12 +1,22 @@
+import 'dart:ui';
+
 import 'package:app/components/player.dart';
+import 'package:app/components/world.dart';
 import 'package:flame/game.dart';
 import '../helpers/direction.dart';
 
 class FlameGameWorld extends FlameGame {
   final Player _player = Player();
+  final World _world = World();
+
   @override
   Future<void> onLoad() async {
+    await add(_world);
     add(_player);
+
+    _player.position = _world.size / 2;
+    camera.followComponent(_player,
+        worldBounds: Rect.fromLTRB(0, 0, _world.size.x, _world.size.y));
   }
 
   void onJoypadDirectionChanged(Direction direction) {
